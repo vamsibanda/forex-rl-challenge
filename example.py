b3737a1a3a79270006e8eb3b2cdea936e3172dc3
kayuksel <46922047+kayuksel@users.noreply.github.com>
2019-02-25 21:58:22 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,13 @@
-import random
+import numpy as np
+import pandas as pd
+#import empyrical as emp
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets
 from tqdm import tqdm as tq
-import os, time, pdb
+import os, time, pdb, random
 import _pickle as cPickle
 import torch.multiprocessing as _mp
 mp = _mp.get_context('spawn')
@@ -24,10 +26,12 @@ train_loader = torch.utils.data.DataLoader(train_dataset,
 test_loader = torch.utils.data.DataLoader(test_dataset, 
     batch_size = 1, shuffle = False, pin_memory = True)
 No_Channels = 12
-No_Proccess = 8
-comm = 1e-4
+No_Proccess = 16
+cost = 1e-4
 
 def calculate_reward(model, loader, index, skip = None):
+    epoch_weights = []
+    epoch_levergs =[]
     #pb = tq(loader, position = index)
     dd = None
     last_action = torch.ones(No_Channels).cuda(dd)
@@ -42,17 +46,36 @@ def calculate_reward(model, loader, index, skip = None):
         weights = torch.tanh(action[:-1])
         certain = 0.5 + torch.sigmoid(action[-1]) / 2.0
         weights = weights / weights.abs().sum()
-        reward = (weights - last_action[:-1]).abs().sum() * comm
+        reward = (weights - last_action[:-1]).abs().sum() * cost
         reward -= (weights * rewards).sum() #- rewards.mean()
         # try risk-sensitive rl e.g. exponential utility
         total_reward = total_reward + (reward / certain)
         last_action[:-1] = weights
         last_action[-1] = certain
+        epoch_weights.append(weights.detach().cpu().numpy())
+        epoch_levergs.append(certain.detach().cpu().numpy())
         torch.cuda.empty_cache()
     #pb.set_postfix({'R': '{:.6f}'.format(total_reward)})
     skipped = 0 if skip is None else sum(skip)
     total_reward = total_reward / (len(loader) - skipped)
-    if skip is None: print('TEST %f' % -total_reward.item())
+    if skip is None: 
+        print('TEST %f' % -total_reward.item())
+        '''
+        lv = np.concatenate(epoch_levergs, axis=0)
+        ew = np.divide(np.concatenate(epoch_weights, axis=0), lv)
+        comm = np.sum(np.abs(ew[1:] - ew[:-1]), axis=1)
+        ret = np.sum(np.multiply(ew, y_test.numpy()), axis=1)[1:]
+        ret = pd.DataFrame(ret - comm * cost)
+        exp = np.exp(ret.resample('1D').sum())-1.0
+        ggg = 'Drawdown:', emp.max_drawdown(exp).values[0], 'Sharpe:', emp.sharpe_ratio(exp)[0], \
+        'Sortino:', emp.sortino_ratio(exp).values[0], 'Stability:', emp.stability_of_timeseries(exp), \
+        'Tail:', emp.tail_ratio(exp), 'ValAtRisk:', emp.value_at_risk(exp)
+        print(' '.join(str(x) for x in ggg))
+        plt.figure()
+        np.exp(ret).cumprod().plot(figsize=(48, 12), title=ttt)
+        plt.savefig('annual_return')
+        plt.close()
+        '''
     return total_reward
 
 def train(model, optimizer, index):
