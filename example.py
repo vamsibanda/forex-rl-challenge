066a8bddb864dd070f0fb57c26f649799b754f1a
kayuksel <46922047+kayuksel@users.noreply.github.com>
2019-02-26 13:25:54 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,14 @@
-'''
+
+import matplotlib
+matplotlib.use('Agg')
 import numpy as np
 import pandas as pd
 import empyrical as emp
-from tqdm import tqdm as tq
-'''
+#from tqdm import tqdm as tq
+import matplotlib.pyplot as plt
+import seaborn as sns
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,6 +18,12 @@ import os, time, pdb, random
 import _pickle as cPickle
 import torch.multiprocessing as _mp
 mp = _mp.get_context('spawn')
+
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
+
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 #with open('weights.pkl', 'rb') as f: weights = cPickle.load(f)
 with open('y_train.pkl', 'rb') as f: y_train = cPickle.load(f)
@@ -31,6 +42,37 @@ No_Features = 512
 No_Channels = 11
 No_Proccess = 20
 cost = 1e-4
+
+def plot_function(epoch_weights):
+    ew = np.concatenate(epoch_weights).reshape(-1, No_Channels)
+    comm = np.sum(np.abs(ew[1:] - ew[:-1]), axis=1)
+    ret = np.sum(np.multiply(ew, y_test.numpy()), axis=1)[1:]
+    ind = pd.date_range("20180101", periods=len(ret), freq='H')
+    ret = pd.DataFrame(ret - comm * cost, index = ind)
+    exp = np.exp(ret.resample('1D').sum()) - 1.0
+    ggg = 'Drawdown:', emp.max_drawdown(exp).values[0], 'Sharpe:', emp.sharpe_ratio(exp)[0], \
+    'Sortino:', emp.sortino_ratio(exp).values[0], 'Stability:', emp.stability_of_timeseries(exp), \
+    'Tail:', emp.tail_ratio(exp), 'ValAtRisk:', emp.value_at_risk(exp)
+    ttt = ' '.join(str(x) for x in ggg)
+    print(ttt)
+    plt.figure()
+    np.exp(ret).cumprod().plot(figsize=(48, 12), title=ttt)
+    plt.savefig('annual_return')
+    plt.close()
+    ret = ret.resample('1W').sum()
+    plt.figure(figsize=(48, 12))
+    pal = sns.color_palette("Greens_d", len(ret))
+    rank = ret.iloc[:,0].argsort()
+    ax = sns.barplot(x=ret.index.strftime('%d-%m'), y=ret.values.reshape(-1), palette=np.array(pal[::-1])[rank])
+    ax.text(0.5, 1.0, ttt, horizontalalignment='center', verticalalignment='top', transform=ax.transAxes)
+    plt.savefig('weekly_weights')
+    plt.close()
+    ew_df = pd.DataFrame(ew)
+    plt.figure(figsize=(48, 12))
+    ax = sns.heatmap(ew_df.T, cmap=cmap, center=0, xticklabels=False, robust=True)
+    ax.text(0.5, 1.0, ttt, horizontalalignment='center', verticalalignment='top', transform=ax.transAxes)
+    plt.savefig('portfolio_weights')
+    plt.close()
 
 def calculate_reward(model, loader, index, skip = None):
     epoch_weights = []
@@ -60,21 +102,7 @@ def calculate_reward(model, loader, index, skip = None):
     total_reward = total_reward / (len(loader) - skipped)
     if skip is None: 
         print('TEST %f' % -total_reward.item())
-        '''
-        ew = np.concatenate(epoch_weights, axis=0)
-        comm = np.sum(np.abs(ew[1:] - ew[:-1]), axis=1)
-        ret = np.sum(np.multiply(ew, y_test.numpy()), axis=1)[1:]
-        ret = pd.DataFrame(ret - comm * cost)
-        exp = np.exp(ret.resample('1D').sum())-1.0
-        ggg = 'Drawdown:', emp.max_drawdown(exp).values[0], 'Sharpe:', emp.sharpe_ratio(exp)[0], \
-        'Sortino:', emp.sortino_ratio(exp).values[0], 'Stability:', emp.stability_of_timeseries(exp), \
-        'Tail:', emp.tail_ratio(exp), 'ValAtRisk:', emp.value_at_risk(exp)
-        print(' '.join(str(x) for x in ggg))
-        plt.figure()
-        np.exp(ret).cumprod().plot(figsize=(48, 12), title=ttt)
-        plt.savefig('annual_return')
-        plt.close()
-        '''
+        plot_function(epoch_weights)
     return total_reward
 
 def train(model, optimizer, index):
@@ -94,7 +122,6 @@ if __name__ == '__main__':
     model.weight.data[:No_Channels,:No_Features] = weights.data
     '''
     optimizer = optim.Adam(params = model.parameters(), lr = 1e-4)
-    torch.backends.cudnn.benchmark = True
     for epoch in range(100):
         model.train(True)
         processes = []
